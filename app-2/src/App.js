cad8de3e5b94d473e164bc7e66146ba69a4379ea
Jessica Myer <36251741+oeddiemeddie@users.noreply.github.com>
2020-01-29 16:08:15 -0700

    bees in your eyes

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hallucinations: ["pink elephant", "top hat man in the back seat", "bees in your eyes", "water", "tree man"]
+    }
+  }
+
+  render() {
+    let hallucinations = this.state.hallucinations.map((element, index) => {
+      return <h1 key={index}>{element}</h1>
+    })
+    return <div className="App">{hallucinations}</div>
+  }
+
 }
 
-export default App;
+export default App
